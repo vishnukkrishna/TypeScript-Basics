39562e5c3452b52165ee49730e15cf1ee641f0ed
vishnukkrishna <vishnukkrishna1999@gmail.com>
2024-10-28 16:32:29 +0530

    Add TypeScript interface example for Details with employee and admin objects

--- index.ts
+++ index.ts
@@ -19,4 +19,30 @@
 
 // .............................................
 
+// Interface
+// interface Details {
+//   name: string;
+//   age: number;
+//   salary: number;
+//   getName: () => void;
+// }
 
+// let employeeDetails: Details = {
+//   name: "Vishnu K",
+//   age: 24,
+//   salary: 150000,
+//   getName() {
+//     console.log(`Name: ${this.name}`);
+//   },
+// };
+
+// let adminDetails: Details = {
+//   name: "Admin",
+//   age: 30,
+//   salary: 100000,
+//   getName() {
+//     console.log(`Age: ${this.age}`);
+//   },
+// };
+
+// .............................................
