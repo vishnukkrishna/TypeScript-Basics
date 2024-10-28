84b930c38404a8a034dcf7b5428f6dbdf1dfe51c
vishnukkrishna <vishnukkrishna1999@gmail.com>
2024-10-28 15:57:59 +0530

    Add TypeScript type examples: Implicit and Explicit Typing

---
+++ index.ts
@@ -0,0 +1,22 @@
+// Infer Types or Imolicit Types (Automatically types generated)
+
+let userName = "Vishnu";
+userName = 0;
+
+// .............................................
+
+// Defining Types or Explicit Types
+
+let userName: string = "Vishnu";
+let age: number = 25;
+let boolean: boolean = true;
+let skills: string[] = ["Python", "Django", "JavaScript", "React JS"];
+let count: number[] = [1, 2, 3, 4, 5];
+let userDetails: { name: string; age: number } = {
+  name: "Vishnu",
+  age: 25,
+};
+
+// .............................................
+
+
